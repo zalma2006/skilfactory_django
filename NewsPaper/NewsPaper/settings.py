58b274b16b44fc3b5d7fc6a34c45b2430b0fbf33
maks <maksim-isaev1989@yandex.ru>
2022-11-22 21:40:40 +0300

    ДЗ 5 Модуль все запросы с shell в файле NewsPaper/D5/D5_dz.txt

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-sg!lgfvm7cq20z=+1t9kgl=t8w248%d*+(r*r6f&i%_5p(mka4"
+with open('sekretkey.txt', 'r') as f:
+    SECRET_KEY = f.read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
